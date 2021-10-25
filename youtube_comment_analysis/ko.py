0769a287fff966c5b9c6a9a60485fcafe9f3430a
ssunggun2 <85274662+ssunggun2@users.noreply.github.com>
2021-10-25 23:42:44 +0900

    33

--- youtube_comment_analysis/ko.py
+++ youtube_comment_analysis/ko.py
@@ -278,3 +278,51 @@ pyLDAvis.display(vis2)
 
 pyLDAvis.save_html(vis2, path2+comment_file+'lda_dic_ko.html')
 
+for i, topic_list in enumerate(ldamodel[corpus]):
+    if i==5:
+        break
+    print(i,'번째 문서의 topic 비율은',topic_list)
+
+
+def make_topictable_per_doc(ldamodel, corpus):
+    topic_table = pd.DataFrame()
+
+    # 몇 번째 문서인지를 의미하는 문서 번호와 해당 문서의 토픽 비중을 한 줄씩 꺼내온다.
+    for i, topic_list in enumerate(ldamodel[corpus]):
+        doc = topic_list[0] if ldamodel.per_word_topics else topic_list            
+        doc = sorted(doc, key=lambda x: (x[1]), reverse=True)
+        # 각 문서에 대해서 비중이 높은 토픽순으로 토픽을 정렬한다.
+        # EX) 정렬 전 0번 문서 : (2번 토픽, 48.5%), (8번 토픽, 25%), (10번 토픽, 5%), (12번 토픽, 21.5%), 
+        # Ex) 정렬 후 0번 문서 : (2번 토픽, 48.5%), (8번 토픽, 25%), (12번 토픽, 21.5%), (10번 토픽, 5%)
+        # 48 > 25 > 21 > 5 순으로 정렬이 된 것.
+
+        # 모든 문서에 대해서 각각 아래를 수행
+        for j, (topic_num, prop_topic) in enumerate(doc): #  몇 번 토픽인지와 비중을 나눠서 저장한다.
+            if j == 0:  # 정렬을 한 상태이므로 가장 앞에 있는 것이 가장 비중이 높은 토픽
+                topic_table = topic_table.append(pd.Series([int(topic_num), round(prop_topic,4), topic_list]), ignore_index=True)
+                # 가장 비중이 높은 토픽과, 가장 비중이 높은 토픽의 비중과, 전체 토픽의 비중을 저장한다.
+            else:
+                break
+    return(topic_table)
+
+
+topictable = make_topictable_per_doc(ldamodel, corpus)
+topictable = topictable.reset_index() # 문서 번호을 의미하는 열(column)로 사용하기 위해서 인덱스 열을 하나 더 만든다.
+topictable.columns = ['문서 번호', '가장 비중이 높은 토픽', '가장 높은 토픽의 비중', '각 토픽의 비중']
+topictable[:10]
+
+topic_word = model.components_ # model.components_also works
+n_top_words = 5   # TOPIC으로 선정될 단어의 수
+
+def get_topic_term_prob(lda_model):
+    topic_term_freqs = lda_model.state.get_lambda()
+    topic_term_prob = topic_term_freqs / topic_term_freqs.sum(axis=1)[:, None]
+    return topic_term_prob
+
+print(ldamodel.alpha.shape) # (n_topics,)
+print(ldamodel.alpha.sum()) # 1.0
+
+topic_term_prob = get_topic_term_prob(ldamodel)
+print(topic_term_prob.shape)     # (n_topics, n_terms)
+print(topic_term_prob[0].sum())  # 1.0
+
